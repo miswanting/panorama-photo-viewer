de3fb00b8e5d3b7bcd92848226ff4c6bd7acf505
(ง •_•)ง <miswanting@users.noreply.github.com>
2019-11-13 14:42:41 +0800

    GOGOGO

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+const http = require('http');
+// import { fstat } from 'fs';
+// import { createServer } from 'http';
+const host = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path')
-module.exports = {
-    entry: './src/main.ts',
+const CopyPlugin = require('copy-webpack-plugin');
+server = {
+    entry: './src/server.ts',
     mode: 'development',
-    watch:true,
+    watch: true,
     output: {
-        path: path.resolve(__dirname, 'static'),
-        filename: 'main.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'server.js'
     },
     module: {
         rules: [
@@ -20,3 +21,32 @@ module.exports = {
         extensions: ['.ts', '.js', '.json'],
     }
 }
+viewer = {
+    entry: './src/index.ts',
+    mode: 'development',
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+    },
+    plugins: [
+        new CopyPlugin([
+            'src/index.html',
+            'src/viewer.html',
+            'res/*.jpg'
+        ])
+    ]
+}
+module.exports = [server, viewer]
